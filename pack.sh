87e01e1e2e1240dba1f26bda48c98bb33f25feff
Rajesh <rajesh23@gmail>
2024-04-20 12:45:02 +0530

    Practicing shell script

--- pack.sh
+++ pack.sh
@@ -7,6 +7,17 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0]
+    then 
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else 
+        echo -e "$2...$G SUCCESS $N"
+    fi 
+
+}
+
 if [ $userid -ne 0 ]
 then 
     echo "Please run with sudo previliges"
@@ -14,4 +25,13 @@ else
     echo "You are a super user"
 fi 
 
-dnf install mysql
+for i in $@
+do
+    echo "package to install $i"
+    dnf list installed $i &>>$logfile
+    if [ $? -eq 0 ]
+    then 
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else 
+        dnf install $i -y &>>$logfile
+        VALIDATE $? "Installation of $i" 
